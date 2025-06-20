5d26948d2a2e59a03b61e012aded38f08ebd689b
kiruthika211005 <2326kb23@kgcas.com>
2025-06-20 14:28:04 +0530

    Create task1.java

---
+++ Day02/task1.java
@@ -0,0 +1,10 @@
+package Tasks;
+public class Task1 {
+public static void main(String[] args) {
+int[] n=new int[7];
+//int[] n= {1,2,3,4,5,6,7};
+for(int i=n.length-1;i>=0;i--) {
+System.out.println(i);
+}
+}
+}
