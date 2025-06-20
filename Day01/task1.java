6ffcc8336da19dce8a3709e75c7006674881fe87
kiruthika211005 <2326kb23@kgcas.com>
2025-06-20 12:51:05 +0530

    Update and rename program3.java to task1.java

--- Day01/task1.java
+++ Day01/task1.java
@@ -1,75 +1,36 @@
 package Day01;
 
-
-
 import java.util.*;
 
-
-
-
-
-
-
-public class program3 {
-
-
-
-
-
-
+public class task1 {
 
 	public static void main(String[] args) {
 
+		for(int a=1;a<=100;a++) {
+            if(a%2==0) {
+            System.out.println(a);
+}
 
-
-	Scanner ref=new Scanner(System.in);	
-
-
+	/*Scanner ref=new Scanner(System.in);	
 
 	System.out.println("Enter starting value.."); 
 
-
-
 	int a=ref.nextInt();
-
-
 
 	System.out.println("Enter ending value.."); 
 
-
-
 	int b=ref.nextInt();
-
-
 
 	for( int i=a; i<b; i++) {
 
+	if (i % 2==0) {
 
-
-		if (i % 2==0) {
-
-
-
-			System.out.println(i);
-
-
-
-		}
-
-
-
+			System.out.println(
 	}
 
-
+	}*/
 
 	}
-
-
-
-
-
-
-
 }
 
 
