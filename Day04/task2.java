ad0b1e58d172306582d7f5605f6e66a7f29d883c
kiruthika211005 <2326kb23@kgcas.com>
2025-06-24 19:09:06 +0530

    Create task2.java

---
+++ Day04/task2.java
@@ -0,0 +1,20 @@
+package Day4;
+
+import java.io.*;
+
+public class Task2 {
+
+	public static void main(String[] args) {
+	String filename="C:\\Users\\TEMP\\Desktop\\WD\\Demo.txt";
+	try(BufferedReader br=new BufferedReader(new FileReader(filename))){
+		String Line;
+		if((Line=br.readLine())!=null) {
+			System.out.println(Line);
+		}
+	}
+	catch(Exception e) {
+		System.out.println("Filenotfound");
+	}
+	}
+
+}
